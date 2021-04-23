f475f55f84ae04416e39ca981163d8c51186253c
Tommy Bergeron <tom@brainpad.org>
2021-04-23 01:45:47 -0400

    cleanup, DRY, now supporting (WIP) multiple keywords, per-user pins and common-pins

--- src/api.js
+++ src/api.js
@@ -20,16 +20,23 @@ const buildKey = (user_id, message_url) => {
   return `${user_id}_${message_url}`;
 }
 
+// TODO: things tend to be fucky when
+// using multiple keywords separated by spaces
+
 module.exports = {
 
   getDump: () => {
     return data;
   },
 
-  pin: (user_id, message_url, keywords) => {
+  pin: (user_id, message_url, keywords, common = true) => {
     try {
-      const key = buildKey(user_id, message_url);
-      data[key] = keywords;
+      if (common) {
+        data[message_url] = keywords;
+      } else {
+        const key = buildKey(user_id, message_url);
+        data[key] = keywords;
+      }
       persist();
     } catch (e) {
       throw e;
@@ -38,10 +45,14 @@ module.exports = {
     }
   },
 
-  unpin: (user_id, message_url) => {
+  unpin: (user_id, message_url, common = true) => {
     try {
-      const key = buildKey(user_id, message_url);
-      delete data[key];
+      if (common) {
+        delete data[message_url];
+      } else {
+        const key = buildKey(user_id, message_url);
+        delete data[key];
+      }
       persist();
     } catch (e) {
       throw e;
@@ -50,15 +61,21 @@ module.exports = {
     }
   },
 
-  search: (user_id, keywords) => {
+  search: (user_id, keywords, common = true) => {
     const filteredPins = Object.entries(data).filter(e => e[1].includes(keywords));
 
-    // removing pins that aren't meant to that user
-    // TODO: find better way; seems sucky and unsafe imo
-    for (let i = 0; i < filteredPins.length; i++) {
-      const pin = filteredPins[i];
-      if (!pin[0].includes(user_id)) {
-        delete filteredPins[i];
+    if (common) {
+
+      // TODO: if common; only get entries WITHOUT user_id attached
+
+    } else {
+      // otherwise removing pins that aren't meant to that user
+      // TODO: find better way; seems sucky and unsafe imo
+      for (let i = 0; i < filteredPins.length; i++) {
+        const pin = filteredPins[i];
+        if (!pin[0].includes(user_id)) {
+          delete filteredPins[i];
+        }
       }
     }
 
--- src/commands.js
+++ src/commands.js
@@ -10,6 +10,17 @@ const reply = (m, c) => {
   m.reply(c);
 };
 
+// convert pins result to string
+const stringifyPins = (pins) => {
+  let stringifiedPins = '';
+  pins.forEach((pin) => {
+    // strip user id
+    const splitString  = pin[0].split('_');
+    stringifiedPins   += `${splitString[1]} --- `;
+  });
+  return stringifiedPins;
+}
+
 // commands
 const ping = (c, a, m) => {
   if (c === 'ping') {
@@ -35,11 +46,29 @@ const pin = (c, a , m) => {
     if ((a[0] !== undefined) && (a[1] !== undefined)) {
       const user_id     = m.author.id;
       const message_url = a[0];
-      const keywords    = a[1]; // TODO: handle multiple arguments
+      const keywords    = a.slice(0, a.length).join(' ');
 
       api.pin(user_id, message_url, keywords);
 
+      reply(m, `Pinned message: **${message_url}** Keywords: **${keywords}**`);
+    } else {
+      reply(m, `${prefix}pin syntax: <message_url> <keywords>`);
+    }
+  }
+};
+
+const userPin = (c, a , m) => {
+  if (c === 'userpin') {
+    if ((a[0] !== undefined) && (a[1] !== undefined)) {
+      const user_id     = m.author.id;
+      const message_url = a[0];
+      const keywords    = a.slice(0, a.length).join(' ');
+
+      api.pin(user_id, message_url, keywords, false);
+
       reply(m, `Pinned message: **${message_url}** for UserId: **${user_id}** Keywords: **${keywords}**`);
+    } else {
+      reply(m, `${prefix}userPin syntax: <message_url> <keywords>`);
     }
   }
 };
@@ -52,33 +81,59 @@ const unpin = (c, a, m) => {
 
       api.unpin(user_id, message_url);
 
-      reply(m, `Unpinned message: **${message_url}** for UserId: **${user_id}**`);
+      reply(m, `Unpinned message: **${message_url}**`);
+    } else {
+      reply(m, `${prefix}unpin syntax: <message_url>`);
     }
   }
 };
 
-const search = (c, a, m) => {
-  if (c === 'searchpin') {
+const userUnpin = (c, a, m) => {
+  if (c === 'userunpin') {
+    if (a[0] !== undefined) {
+      const user_id     = m.author.id;
+      const message_url = a[0];
+
+      api.unpin(user_id, message_url, false);
+
+      reply(m, `Unpinned message: **${message_url}** for UserId: **${user_id}**`);
+    } else {
+      reply(m, `${prefix}userUnpin syntax: <message_url>`);
+    }
+  }
+};
+
+const searchPins = (c, a, m) => {
+  if (c === 'searchpins') {
+    if (a[0] !== undefined) {
+      const user_id     = m.author.id;
+      const keywords    = a.join(' ');
+
+      // fetching common pins based on keywords
+      const pins = api.search(user_id, keywords);
+
+      reply(m, `Searching in pins for **${keywords}** for UserId: **${user_id}**. Result: ${stringifyPins(pins)}`);
+    } else {
+      reply(m, `${prefix}searchPins syntax: <keywords>`);
+    }
+  }
+};
+
+const userSearchPins = (c, a, m) => {
+  if (c === 'usersearchpins') {
     if (a[0] !== undefined) {
       const user_id     = m.author.id;
       const keywords    = a[0];
 
-      const pins = api.search(user_id, keywords);
+      // fetching per-user pins based on keywords
+      const pins = api.search(user_id, keywords, false);
 
-      let stringifiedPins = '';
-      pins.forEach((pin) => {
-        // strip user id
-        const splitString = pin[0].split('_');
-        stringifiedPins += `${splitString[1]} --- `;
-      });
-
-      reply(m, `Searching in pins for **${keywords}** for UserId: **${user_id}**. Result: ${stringifiedPins}`);
+      reply(m, `Searching in pins for **${keywords}** for UserId: **${user_id}**. Result: ${stringifyPins(pins)}`);
+    } else {
+      reply(m, `${prefix}userSearchPins syntax: <keywords>`);
     }
   }
 };
-
-// TODO: make unPin command
-// TODO: make searchPin command
 
 // exposed command implementation
 module.exports = (message) => {
@@ -95,10 +150,13 @@ module.exports = (message) => {
   console.log('[command incoming]:', command, args);
 
   // hooking commands
-  ping    (command, args, message);
-  getArgs (command, args, message);
-  getDump (command, args, message);
-  pin     (command, args, message);
-  unpin   (command, args, message);
-  search  (command, args, message);
+  ping           (command, args, message);
+  getArgs        (command, args, message);
+  getDump        (command, args, message);
+  pin            (command, args, message);
+  unpin          (command, args, message);
+  userPin        (command, args, message);
+  userUnpin      (command, args, message);
+  searchPins     (command, args, message);
+  userSearchPins (command, args, message);
 };
